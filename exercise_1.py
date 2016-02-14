90b0d62be374beb7c893725683160f29881d3b2d
Lene Preuss <lene.preuss@gmail.com>
2016-02-14 16:26:34 +0100

    i got a result! (sort of. csv data are printed. nothing else. but hey.)

--- exercise_1.py
+++ exercise_1.py
@@ -63,10 +63,44 @@ def graphlab_exercise():
     print ('bedroom test rss', bedroom_test_rss)
     print ('ratio', sqft_test_rss/bedroom_test_rss)
 
+
+def print_tensor(tensor):
+    print(str(tensor.dtype) + ' Tensor ' + tensor.name + ' ' + str(tensor.get_shape()))
+
+
+def print_tensors(tensors):
+    for tensor in tensors:
+        print_tensor(tensor)
+
+
 def read_csv(file_name):
     reader = tf.TextLineReader(skip_header_lines=1)
     filename_queue = tf.train.string_input_producer([file_name])
     key, value = reader.read(filename_queue)
+    #                  id,   date, price,bedrooms,bathrooms,sqft_living,sqft_lot,floors,waterfront,view,condition,grade,sqft_above,sqft_basement,yr_built,yr_renovated,zipcode,lat,long,sqft_living15,sqft_lot15
+    record_defaults = [[1.], [1.], [1.]]
+
+    col1, col2, col3 = tf.decode_csv(value, record_defaults=record_defaults)
+    print_tensors([col1, col2, col3])
+
+    features = col2 # tf.concat(0, [col1, col2])
+    print_tensor(features)
+
+    with tf.Session() as sess:
+        # Start populating the filename queue.
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        for i in range(10):
+            # Retrieve a single instance:
+            example, label = sess.run([features, col3])
+            print('example, label:', example, label)
+
+    coord.request_stop()
+    coord.join(threads)
+
     return key, value
 
-print(read_csv('data/kc_house_data.csv'))
+c = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+
+print(read_csv('data/dummy_test_data.csv'))
